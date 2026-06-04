d7a086d647bfa4b10f2d0f56ada389a50d8fb9d9
dedalo-integraciones <dedalo.integraciones@gmail.com>
2026-06-04 15:26:34 -0300

    Initial commit - Banquetes Loyale

--- Primera subida.bat
+++ Primera subida.bat
@@ -8,7 +8,7 @@ git init
 git add .
 
 :: Crea el primer commit (confirmación) con todos los archivos
-git commit -m "Initial commit - Banquetes Loyal"
+git commit -m "Initial commit - Banquetes Loyale"
 
 :: Renombra la rama principal a 'main'
 git branch -M main
@@ -16,10 +16,10 @@ git branch -M main
 :: Vincula tu carpeta local con el repositorio remoto de GitHub
 git remote add origin https://github.com/dedalo-integraciones/banquetesloyale.git
 
-:: Sube los archivos a GitHub
+:: Sube los archivos a GitHub (forzando la subida para evitar conflictos iniciales)
 echo.
 echo Subiendo archivos a GitHub...
-git push -u origin main
+git push -u origin main --force
 
 echo.
 echo Proceso terminado. Revisa tu repositorio en GitHub.
